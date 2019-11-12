66279e292409df20eb8139f4fa83e10c3c308093
Patryk Pastecki <patryk.pastecki@gmail.com>
2019-11-12 17:00:32 +0100

    Add entities

---
+++ CryptoPrices.Core/Entities/Cryptocurrency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoPrices.Core.Entities
+{
+    public class CryptoCurrency
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Symbol { get; set; }
+
+        public decimal? MaxSupply { get; set; }
+
+        public decimal? CirculatingSupply { get; set; }
+
+        public int Rank { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
--- CryptoPrices.Service/Program.cs
+++ CryptoPrices.Service/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace CryptoPrices.Service
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace CryptoPrices.Service
 {
     public static class Program
     {
+        private const string ApiKey = "ea8c99fb-49ac-4621-a366-ce3a00f244b3";
+
         public static void Main(string[] args)
         {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("X-CMC_PRO_API_KEY", ApiKey);
+                client.Headers.Add("Accepts", "application/json");
+
+                var url = new UriBuilder("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+                url.Query = $"start=1&limit=5000&convert=USD";
+
+                var result = client.DownloadString(url.ToString());
+                File.WriteAllText(@"c:\src\prices.json", result);
+            }
         }
     }
 }
